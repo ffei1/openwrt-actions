7f2a2c9f31ca26d18710a2da0be8e41972c9c5ce
ffei1 <166067404+ffei1@users.noreply.github.com>
2025-06-12 18:05:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,13 +21,19 @@ rm target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 rm package/boot/uboot-rockchip/Makefile
 rm package/boot/uboot-rockchip/patches/900-arm-add-dts-files.patch
 rm target/linux/rockchip/patches-6.6/900-arm64-boot-add-dts-files.patch
-wget -P package/boot/uboot-rockchip/src/arch/arm/dts https://raw.githubusercontent.com/ffei1/openwrt-actions/main/uboot/rk3399-fine-3399.dts
-wget -P target/linux/rockchip/files/arch/arm64/boot/dts/rockchip https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/rk3399-fine-3399.dts
-wget -P package/boot/uboot-rockchip/src/arch/arm/dts https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/rk3399-fine-3399-u-boot.dtsi
 wget -P target/linux/rockchip/image https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/armv8.mk
 wget -P target/linux/rockchip/armv8/base-files/etc/board.d https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/02_network
 wget -P target/linux/rockchip/armv8/base-files/etc/hotplug.d/net https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/40-net-smp-affinity
-wget -P package/boot/uboot-rockchip/src/configs https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/fine-3399-rk3399_defconfig
 wget -P package/boot/uboot-rockchip https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/Makefile
 wget -P package/boot/uboot-rockchip/patches https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/900-arm-add-dts-files.patch
 wget -P target/linux/rockchip/patches-6.6 https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/900-arm64-boot-add-dts-files.patch
+#fine3399
+wget -P package/boot/uboot-rockchip/src/arch/arm/dts https://raw.githubusercontent.com/ffei1/openwrt-actions/main/uboot/rk3399-fine-3399.dts
+wget -P package/boot/uboot-rockchip/src/arch/arm/dts https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/rk3399-fine-3399-u-boot.dtsi
+wget -P package/boot/uboot-rockchip/src/configs https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/fine-3399-rk3399_defconfig
+wget -P target/linux/rockchip/files/arch/arm64/boot/dts/rockchip https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/rk3399-fine-3399.dts
+#lubancat3588
+wget -P package/boot/uboot-rockchip/src/arch/arm/dts https://raw.githubusercontent.com/ffei1/openwrt-actions/main/uboot/rk3588-lubancat-5.dts
+wget -P package/boot/uboot-rockchip/src/arch/arm/dts https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/rk3588-lubancat-5-u-boot.dtsi
+wget -P package/boot/uboot-rockchip/src/configs https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/lubancat-5-rk3588_defconfig
+wget -P target/linux/rockchip/files/arch/arm64/boot/dts/rockchip https://raw.githubusercontent.com/ffei1/openwrt-actions/main/33/rk3588-lubancat-5.dts
